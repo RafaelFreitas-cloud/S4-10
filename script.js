308ef85041a376220bb0d9d3e0f88a6a1be09789
RafaelFreitas <raf.mec.ba@gmail.com>
2022-08-31 10:13:50 -0300

    S4-10 final

---
+++ script.js
@@ -0,0 +1,13 @@
+// ------------Exercício 1-----------------
+let nomeDosCompetidores = ["Maria", "Heric", "Roberta", "Rafael", "Lucas"]
+console.log(`1ª posição: ${nomeDosCompetidores[0]}`)
+
+// ------------Exercício 2-----------------
+console.log(`5ª posição: ${nomeDosCompetidores[4]}`)
+
+// ------------Exercício 2-----------------
+console.log(`1ª posição: ${nomeDosCompetidores[0]}`)
+console.log(`2ª posição: ${nomeDosCompetidores[1]}`)
+console.log(`3ª posição: ${nomeDosCompetidores[2]}`)
+console.log(`4ª posição: ${nomeDosCompetidores[3]}`)
+console.log(`5ª posição: ${nomeDosCompetidores[4]}`)
